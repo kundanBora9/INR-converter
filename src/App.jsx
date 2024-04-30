f38e7671a29e80966dee862140957185778f12ff
kundanBora9 <kb926464@gmail.com>
2024-04-30 21:03:56 +0530

    designing-part

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import InputBox from "./components/InputBox"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
   <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
   style={{backgroundImage:`url(https://plus.unsplash.com/premium_photo-1681437744904-d815bc19c2f8?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`}}>
-
+ <InputBox/>
   </div>
   )
 }
